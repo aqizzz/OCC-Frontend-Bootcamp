8811c6cf121c23a6eeeac0bef88c1529594d10bd
Fei <aqizzz@gmail.com>
2023-12-05 01:06:27 -0500

    Added JavaScript files

---
+++ 5-JavaScript/app.js
@@ -0,0 +1,20 @@
+//String
+let name = "John";
+console.log("The type of name:", typeof name);
+
+//Number
+let age = 25;
+console.log("The type of age:", typeof age);
+
+//Boolean -True or false
+let isStudent = false;
+let isTeacher = true;
+console.log("The type of isStudent:", typeof isStudent);
+console.log("The type of isTeacher:", typeof isTeacher);
+
+//Null - Intentional absence of data
+let girlFriend = null;
+console.log("The type of girlfriend:", typeof girlFriend);
+
+let city;
+console.log("The type of city:", typeof city);
